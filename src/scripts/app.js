c98907f20d9a635a077752f9e0fdc0202b0ac343
Noemie <nonomillaire@gmail.com>
2023-05-17 15:59:13 +0200

    burger menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,15 @@
-console.info('Hello world');
+"use strict"
+
+
+//burger menu
+const menuBurger = document.querySelector('.menu__icon');
+const menu = document.querySelector('.nav')
+const menu__lien = document.querySelector('.menu__el')
+
+menuBurger.addEventListener('click', function(){
+    menu.classList.toggle('menu--open');
+})
+
+menu__lien.addEventListener('click', function(){
+    menu.classList.remove('menu--open');
+})
